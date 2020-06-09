84377ce676521a5ed3813f0e976c4252be26e9c2
Romaric Tsopnang <tsopnangsr@gmail.com>
2020-06-09 17:13:43 +0100

    Adding 4 motor and testing

--- obstacle-detector-robot-code/arduino-motor-driver-code/arduino-motor-driver-code.ino
+++ obstacle-detector-robot-code/arduino-motor-driver-code/arduino-motor-driver-code.ino
@@ -5,9 +5,12 @@
 #include <AFMotor.h>
 
 // DC motor on M2
-AF_DCMotor motor(2);
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
 // DC hobby servo
-Servo servo1;
+
 
 
 void setup() {
@@ -15,18 +18,37 @@ void setup() {
   Serial.println("Motor party!");
   
    
+  // turn on motor #1
+  motor1.setSpeed(200);
+  motor1.run(RELEASE);
+  
   // turn on motor #2
-  motor.setSpeed(200);
-  motor.run(RELEASE);
+  motor2.setSpeed(200);
+  motor2.run(RELEASE);
+  
+  // turn on motor #3
+  motor3.setSpeed(200);
+  motor3.run(RELEASE);
+  
+  // turn on motor #2
+  motor4.setSpeed(200);
+  motor4.run(RELEASE);
 }
 
 int i;
 
 // Test the DC motor, stepper and servo ALL AT ONCE!
 void loop() {
-  motor.run(FORWARD);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);  
   delay(3000);
-  motor.run(RELEASE);
+  
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
   delay(3000);  
   
 }
